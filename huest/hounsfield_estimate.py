2902595bd884e86b63ac799975b7bb6f9fd9cabd
Jonas Adler <jonasadl@kth.se>
2016-10-06 16:31:06 +0200

    Add silencing of xraylib spam

--- huest/hounsfield_estimate.py
+++ huest/hounsfield_estimate.py
@@ -2,8 +2,44 @@
 
 import numpy as np
 import xraylib as xrl
+import os
+import sys
 
 __all__ = ('hounsfield_value', 'density')
+
+
+class Silence:
+    """Context manager to silence C output that xraylib spams."""
+    def __enter__(self):
+        # save previous stdout/stderr
+        self.saved_streams = sys.__stdout__, sys.__stderr__
+        self.fds = fds = [s.fileno() for s in self.saved_streams]
+        self.saved_fds = map(os.dup, fds)
+        # flush any pending output
+        for s in self.saved_streams:
+            s.flush()
+
+        # open surrogate files
+        null_streams = [open(f, 'w', 0) for f in [os.devnull, os.devnull]]
+        self.null_fds = [s.fileno() for s in null_streams]
+        self.null_streams = null_streams
+
+        # overwrite file objects and low-level file descriptors
+        map(os.dup2, self.null_fds, fds)
+
+    def __exit__(self, *args):
+        # flush any pending output
+        for s in self.saved_streams:
+            s.flush()
+        # restore original streams and file descriptors
+        map(os.dup2, self.saved_fds, self.fds)
+        sys.stdout, sys.stderr = self.saved_streams
+        # clean up
+        for s in self.null_streams:
+            s.close()
+        for fd in self.saved_fds:
+            os.close(fd)
+        return False
 
 
 def hounsfield_value(formula, density, energies, spectrum=None):
@@ -33,14 +69,16 @@ def hounsfield_value(formula, density, energies, spectrum=None):
             raise ValueError('shape of `energies` and `spectrum` should match')
     spectrum /= spectrum.sum()
 
-    attenuation = np.array([xrl.CS_Total_CP(formula, energy)
-                            for energy in energies])
+    with Silence():
+        attenuation = np.array([xrl.CS_Total_CP(formula, energy)
+                                for energy in energies])
 
     if np.any(attenuation == 0):
         raise ValueError('invalid formula')
 
-    attenuation_water = np.array([xrl.CS_Total_CP('Water, Liquid', energy)
-                                  for energy in energies])
+    with Silence():
+        attenuation_water = np.array([xrl.CS_Total_CP('Water, Liquid', energy)
+                                      for energy in energies])
 
     mean_att = np.sum(attenuation * spectrum)
     mean_water_att = np.sum(attenuation_water * spectrum)
